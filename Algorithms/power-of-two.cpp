917175aaa3a53cdd2355290141ee969890010145
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2016-04-18 05:53:50 -0500

    Create power-of-two.cpp

---
+++ Algorithms/power-of-two.cpp
@@ -0,0 +1,16 @@
+//231. Power of Two
+//https://leetcode.com/problems/power-of-two/
+//http://www.cnblogs.com/grandyang/p/4623394.html
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+	bool isPowerOfTwo(int n) {
+		return n > 0 && !(n & (n - 1));
+	}
+};
+int main(int argc, char** argv) {
+	Solution solution;
+	getchar();
+	return 0;
+}
