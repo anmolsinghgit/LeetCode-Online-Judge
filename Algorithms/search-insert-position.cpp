04210daf148231aa0f78cc2a9b2338af62a0399b
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2016-05-12 00:15:29 -0500

    Create search-insert-position.cpp

---
+++ Algorithms/search-insert-position.cpp
@@ -0,0 +1,33 @@
+//35. Search Insert Position
+//https://leetcode.com/problems/search-insert-position/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+class Solution {
+public:
+	int searchInsert(const vector<int>& nums, const int& target) {
+		int result = 0;
+		if (nums.back() < target)
+			result = nums.size();
+		else {
+			auto first = lower_bound(nums.begin(), nums.end(), target);
+			result = distance(nums.begin(), first);
+		}
+		return result;
+	}
+};
+int main(void) {
+	Solution solution;
+
+	vector<int> nums = { 1,3,5,6 };
+	//int target = 5;
+	//int target = 2;
+	//int target = 7;
+	int target = 0;
+	cout << solution.searchInsert(nums, target);
+	cout << "\nPassed\n";
+	getchar();
+	return 0;
+}
