1506ee137493d34fd83a54340a2340ce190f5d59
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2016-06-20 17:21:08 -0500

    Update combination-sum-ii.cpp

--- Algorithms/combination-sum-ii.cpp
+++ Algorithms/combination-sum-ii.cpp
@@ -7,53 +7,47 @@
 using namespace std;
 class Solution {
 public:
-	vector<vector<int>> combinationSum2(vector<int>& candidates, const int& target) {
+	vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
 		vector<vector<int>> result;
-		if (target <= 0 || candidates.empty())
-			return result;
 		sort(begin(candidates), end(candidates));
-		if (target < candidates.front())
-			return result;
-		auto lb = lower_bound(begin(candidates), end(candidates), target);
-		if (lb < end(candidates) && *lb == target)
-			result = {{target}};
-		for (auto it = begin(candidates); it < lb; ++it) {
-			vector<int> t(begin(candidates), end(candidates));
-			t.erase(begin(t) + distance(begin(candidates), it));
-			vector<vector<int>> x = this->combinationSum2(t, target - *it);
-			if (x.empty())
-				continue;
+		vector<int> path;
+		this->combinationSum2(0, path, target, result, candidates);
+		return result;
+	}
+	void combinationSum2(int start, vector<int>& path, int target, vector<vector<int>>& result, const vector<int>& candidates) {
+		if (target < 0) 
+			return;
+		else if (target == 0) {
+			if (path.empty())
+				return;
 			else {
-				for (auto& i : x) {
-					auto pos = lower_bound(begin(i), end(i), *it);
-					i.insert(pos, *it);
-					if (find(begin(result), end(result), i) == end(result))
-						result.push_back(i);
-					else
-						continue;
-				}
+				result.push_back(path);
+				return;
 			}
 		}
-		return result;
+		else {
+			for (int i = start; i < candidates.size(); i++) {
+				if (i > start && candidates[i] == candidates[i - 1])
+					continue;
+				path.push_back(candidates[i]);
+				this->combinationSum2(i + 1, path, target - candidates[i], result, candidates);
+				path.pop_back();
+			}
+			return;
+		}
+
 	}
 };
 int main(void) {
 	Solution solution;
-	vector<int> candidates = {10,1,2,7,6,1,5};
-	int target = 8;
-	cout << '[';
+	vector<int> candidates = {14,22,8,31,30,26,9,34,20,13,10,22,6,12,18,22,28,19,14,17,24,24,22,14,27,30,6,23,25,14,33,5,23,7,23,18,28,20,9,5,20,14,22,21,21,6,9,6,12,10,19,31,21,28,32,14,23,33,32,14};
+	int target = 29;
 	for (const auto& i : solution.combinationSum2(candidates, target)) {
-		cout << '\n';
-		cout << '\t';
-		cout << '[';
-		cout << '\t';
 		for (const auto& j : i) {
 			cout << j << '\t';
 		}
-		cout << ']';
 		cout << '\n';
 	}
 	cout << "\nPassed\n";
-	getchar();
 	return 0;
 }
