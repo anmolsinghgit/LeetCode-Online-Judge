06729410f33edc7d614a3f56282771acbeb8220b
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-07-07 16:55:42 -0400

    Update

--- Algorithms/jump-game-ii.cpp
+++ Algorithms/jump-game-ii.cpp
@@ -7,17 +7,18 @@ public:
 		int result(0);
 		int i(0);
 		while (i < nums.size() - 1) {
-			cout << i << '\n';
 			if (i + nums[i] >= nums.size() - 1) {
 				++result;
 				break;
 			}
 			else {
-				int upperbound(i + 1 + nums[i + 1]);
-				int nexti(i + 1);
+				int upperbound(-1);
+				int nexti(-1);
 				for (int j(1); j <= nums[i]; ++j) {
-					if (i + j + nums[i + j] > upperbound)
+					if (i + j + nums[i + j] > upperbound) {
+						upperbound = i + j + nums[i + j];
 						nexti = i + j;
+					}
 				}
 				i = nexti;
 				++result;
@@ -30,9 +31,9 @@ int main(void) {
 	Solution solution;
 	vector<vector<int>> A;
 	vector<int> b;
-	//A.push_back(vector<int>({ 2,3,1,1,4 }));
+	A.push_back(vector<int>({ 2,3,1,1,4 }));
 	A.push_back(vector<int>({ 4,1,1,3,1,1,1 }));
-	//b.push_back(2);
+	b.push_back(2);
 	b.push_back(2);
 	for (size_t i(0); i < b.size(); ++i) {
 		if (b[i] ^ solution.jump(A[i])) {
