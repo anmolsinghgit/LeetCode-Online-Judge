d44e3b2216fe8da4d6450d4c2cc7f58ac603bc46
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-07-09 00:06:50 -0400

    Upload 52. N-Queens II

--- Algorithms/n-queens-ii.cpp
+++ Algorithms/n-queens-ii.cpp
@@ -2,12 +2,41 @@
 // https://leetcode.com/problems/n-queens-ii/
 // https://github.com/soulmachine/leetcode
 #include <iostream>
+#include <vector>
 using namespace std;
 class Solution {
 public:
-    int totalNQueens(const int& n) {
-        return 0;
-    }
+	int totalNQueens(const int& n) {
+		int result(0);
+		vector<int> column(n, 0);
+		vector<int> mainDiagonal(2 * n - 1, 0);
+		vector<int> antiDiagonal(2 * n - 1, 0);
+		this->totalNQueens(0, n, result, column, mainDiagonal, antiDiagonal);
+		return result;
+	}
+private:
+	void totalNQueens(const int& r, const int& n, int& result, vector<int>& column, vector<int>& mainDiagonal, vector<int>& antiDiagonal) {
+		if (r == n) {
+			++result;
+			return;
+		}
+		else {
+			for (int i(0); i < n; ++i) {
+				if (!column[i] && !mainDiagonal[r + i] && !antiDiagonal[n - 1 - r + i]) {
+					column[i] = 1;
+					mainDiagonal[r + i] = 1;
+					antiDiagonal[n - 1 - r + i] = 1;
+					this->totalNQueens(r + 1, n, result, column, mainDiagonal, antiDiagonal);
+					column[i] = 0;
+					mainDiagonal[r + i] = 0;
+					antiDiagonal[n - 1 - r + i] = 0;					
+				}
+				else
+					continue;
+			}
+			return;
+		}
+	}
 };
 int main(void) {
 	Solution solution;
@@ -15,12 +44,16 @@ int main(void) {
 	vector<int> b;
 	a.push_back(1);
 	b.push_back(1);
+	a.push_back(2);
+	b.push_back(0);
 	for (int i(0); i < b.size(); ++i) {
 		if (b[i] ^ a[i]) {
 			cout << "\nError\n";
+			getchar();
 			return 0;
 		}
 	}
 	cout << "\nPassed\n";
+	getchar();
 	return 0;
 }
