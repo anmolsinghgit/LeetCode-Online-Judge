693479321f4f6f4eed462455724ef992b9f70c1a
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-07-26 07:36:53 -0500

    1. Two Sum

--- Algorithms/two-sum.cpp
+++ Algorithms/two-sum.cpp
@@ -4,42 +4,33 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <unordered_map>
 using namespace std;
 class Solution {
 public:
-	vector<int> twoSum(vector<int>& nums, int target) {
-		vector<int> index;
-		index.resize(2);
-		vector<int> sortedNums(nums);
-		sort(sortedNums.begin(), sortedNums.end());
-		int i = 0;
-		int j = sortedNums.size() - 1;
-		while ((sortedNums[i] + sortedNums[j]) != target) {
-			if ((sortedNums[i] + sortedNums[j]) < target) {
-				++i;
+	vector<int> twoSum(const vector<int>& nums, const int& target) {
+		unordered_map<int, int> hashmap;
+		vector<int> result(2, 0);
+		for (int i = 0; i < nums.size(); ++i) {
+			int rest = target - nums[i];
+			if (hashmap.count(rest)) {
+				result[0] = hashmap[rest];
+				result[1] = i;
+				return result;
 			}
-			else {
-				--j;
-			}
+			hashmap[nums[i]] = i;
 		}
-		vector<int>::iterator it;
-		vector<int>::iterator jt;
-		it = find(nums.begin(), nums.end(), sortedNums[i]);
-		jt = find(nums.begin(), it, sortedNums[j]);
-		if (jt == it) {
-			jt = find(it + 1, nums.end(), sortedNums[j]);
-		}
-		int index_i;
-		index_i = distance(nums.begin(), it);
-		int index_j;
-		index_j = distance(nums.begin(), jt);
-		index[0] = min(index_i, index_j);
-		index[1] = max(index_i, index_j);
-		return index;
+		return result;
 	}
 };
 int main(int argc, char** argv) {
 	Solution solution;
-	getchar();
+	vector<int> nums = {2, 7, 11, 15};
+	int target = 9;
+	for (const auto& i : solution.twoSum(nums,  target)) {
+		cout << i << '\t';
+	}
+	cout << "\nPassed\n";
+	cout << "\nPassed All\n";	
 	return 0;
 }
