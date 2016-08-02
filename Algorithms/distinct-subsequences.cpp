1dee8b8dbe61cc61839a243f49b384150d733bf0
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-08-02 07:14:15 -0500

    115. Distinct Subsequences

--- Algorithms/distinct-subsequences.cpp
+++ Algorithms/distinct-subsequences.cpp
@@ -14,7 +14,7 @@ public:
 		vector<vector<int>> OPT(m + 1, vector<int>(n + 1, 0));
 		for (size_t i = 0; i < m + 1; ++i) OPT[i][0] = 1;
 		for (size_t i = 1; i < m + 1; ++i) {
-			for (int j = 1; j < n + 1; ++j) {
+			for (size_t j = 1; j < n + 1; ++j) {
 				if (s[i - 1] == t[j - 1]) OPT[i][j] = OPT[i - 1][j - 1] + OPT[i - 1][j];
 				else OPT[i][j] = OPT[i - 1][j];
 			}
@@ -45,6 +45,5 @@ int main(void) {
 		return 0;
 	}
 	cout << "\nPassed All\n";
-	getchar();
 	return 0;
 }
