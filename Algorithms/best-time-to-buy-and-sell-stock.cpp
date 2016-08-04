8e4188293c345de112527b5be8482d8549f8d1a0
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-08-04 11:45:14 -0400

    121. Best Time to Buy and Sell Stock

---
+++ Algorithms/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,51 @@
+// 121. Best Time to Buy and Sell Stock
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+class Solution {
+public:
+	int maxProfit(const vector<int>& prices) {
+		return this->maxProfit(1, prices);
+	}
+private:
+	int maxProfit(const int& k, const vector<int>& prices) {
+		if (prices.empty()) return 0;
+		const int n = prices.size();
+		if (k >= n / 2) {
+			int result = 0;
+			for (size_t i = 1; i < prices.size(); ++i) result += max(0, prices[i] - prices[i - 1]);
+			return result;
+		}
+		vector<int> buy(k + 1, INT_MIN);
+		vector<int> sell(k + 1, 0);
+		for (const auto& price : prices) {
+			for (int i = 1; i < k + 1; ++i) {
+				buy[i] = max(buy[i], sell[i - 1] - price);
+				sell[i] = max(sell[i], buy[i] + price);
+			}
+		}
+		return sell[k];
+	}
+};
+int main(void) {
+	Solution solution;
+	vector<int> prices = {7, 1, 5, 3, 6, 4};
+	if (solution.maxProfit(prices) == 5) {
+		cout << "\nPassed\n";
+	} else {
+		cout << "\nError\n";
+		return 0;
+	}
+	prices = {7, 6, 4, 3, 1};
+	if (solution.maxProfit(prices) == 0) {
+		cout << "\nPassed\n";
+	} else {
+		cout << "\nError\n";
+		return 0;
+	}
+	cout << "\nPassed All\n";
+	return 0;
+}
