bccb0929030bd08abf50a79328733556e9f7331f
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-08-04 11:55:03 -0400

    121. Best Time to Buy and Sell Stock

--- Algorithms/best-time-to-buy-and-sell-stock.cpp
+++ Algorithms/best-time-to-buy-and-sell-stock.cpp
@@ -1,5 +1,6 @@
 // 121. Best Time to Buy and Sell Stock
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// https://discuss.leetcode.com/topic/52580/java-solution-in-o-n-time-and-o-k-space/2
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -11,10 +12,9 @@ public:
 		return this->maxProfit(1, prices);
 	}
 private:
-	int maxProfit(const int& k, const vector<int>& prices) {
+	int maxProfit(const size_t& k, const vector<int>& prices) {
 		if (prices.empty()) return 0;
-		const int n = prices.size();
-		if (k >= n / 2) {
+		if (k >= prices.size() / 2) {
 			int result = 0;
 			for (size_t i = 1; i < prices.size(); ++i) result += max(0, prices[i] - prices[i - 1]);
 			return result;
@@ -22,7 +22,7 @@ private:
 		vector<int> buy(k + 1, INT_MIN);
 		vector<int> sell(k + 1, 0);
 		for (const auto& price : prices) {
-			for (int i = 1; i < k + 1; ++i) {
+			for (size_t i = 1; i < k + 1; ++i) {
 				buy[i] = max(buy[i], sell[i - 1] - price);
 				sell[i] = max(sell[i], buy[i] + price);
 			}
