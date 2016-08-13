aeb8626a68f1671f91171effa6b3ea576f63cbbe
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-08-13 19:36:21 -0400

    158. Read N Characters Given Read4 II - Call multiple times

--- Algorithms/read-n-characters-given-read4-ii-call-multiple-times.cpp
+++ Algorithms/read-n-characters-given-read4-ii-call-multiple-times.cpp
@@ -1,5 +1,6 @@
 // 158. Read N Characters Given Read4 II - Call multiple times
 // https://leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times/
+// https://discuss.leetcode.com/topic/7094/a-simple-java-code/4
 // http://www.cnblogs.com/EdwardLiu/p/4240616.html
 // Forward declaration of the read4 API.
 int read4(char *buf);
@@ -12,8 +13,19 @@ public:
 	* @return    The number of characters read
 	*/
 	int read(char *buf, int n) {
-		
+		int result = 0;
+		while (result < n) {
+			if (cachePtr == 0) cacheCnt = read4(cache);
+			if (cacheCnt == 0) break;
+			while (result < n && cachePtr < cacheCnt) buf[result++] = cache[cachePtr++];
+			if (cachePtr >= cacheCnt) cachePtr = 0;
+		}
+		return result;
 	}
+private:
+	char *cache = new char[4];
+	int cacheCnt = 0;
+	int cachePtr = 0;
 };
 
 // "ab"
