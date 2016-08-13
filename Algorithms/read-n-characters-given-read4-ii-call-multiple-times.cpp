58f61aca7912612a6707cd0c9b7381af862d0d01
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-08-12 23:43:54 -0400

    157. Read N Characters Given Read4

---
+++ Algorithms/read-n-characters-given-read4-ii-call-multiple-times.cpp
@@ -0,0 +1,20 @@
+// 158. Read N Characters Given Read4 II - Call multiple times
+// https://leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times/
+// http://www.cnblogs.com/EdwardLiu/p/4240616.html
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
+public:
+	/**
+	* @param buf Destination buffer
+	* @param n   Maximum number of characters to read
+	* @return    The number of characters read
+	*/
+	int read(char *buf, int n) {
+		
+	}
+};
+
+// "ab"
+// [read(1),read(2)]
