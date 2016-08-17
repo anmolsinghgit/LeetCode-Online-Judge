fa20d48c92239bff0592242310af1c0d16389c03
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-08-17 17:42:00 -0400

    200. Number of Islands

---
+++ Algorithms/number-of-islands.cpp
@@ -0,0 +1,53 @@
+// 200. Number of Islands
+// https://leetcode.com/problems/number-of-islands/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	int numIslands(vector<vector<char>>& grid) {
+		for (int i = 0; i < grid.size(); ++i) {
+			for (int j = 0; j < grid.front().size(); ++j) {
+				if (grid[i][j] == '1') {
+					grid[i][j] = 'X';
+					this->DFS(i - 1, j, grid);
+					this->DFS(i + 1, j, grid);
+					this->DFS(i, j - 1, grid);
+					this->DFS(i, j + 1, grid);
+				}
+			}
+		}
+		int result = 0;
+		for (const auto &i : grid) {
+			for (const auto &j : i) {
+				cout << j << '\t';
+				if (j == 'X') ++result;
+			}
+			cout << '\n';
+		}
+		return result;
+	}
+private:
+	void DFS(int i, int j, vector<vector<char>>& grid) {
+		if (i < 0 || i >= grid.size() || j < 0 || j >= grid.front().size()) return;
+		if (grid[i][j] == '0') return;
+		if (grid[i][j] == 'X') return;
+		grid[i][j] = '0';
+		this->DFS(i - 1, j, grid);
+		this->DFS(i + 1, j, grid);
+		this->DFS(i, j - 1, grid);
+		this->DFS(i, j + 1, grid);
+		return;
+	}
+};
+int main(void) {
+	Solution solution;
+	vector<vector<char>> grid = {{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}};
+	cout << solution.numIslands(grid) << "\tPassed\n";
+	grid = {{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}};
+	cout << solution.numIslands(grid) << "\tPassed\n";
+	grid = {{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}};
+	cout << solution.numIslands(grid) << "\tPassed\n";
+	cout << "\nPassed All\n";
+	return 0;
+}
