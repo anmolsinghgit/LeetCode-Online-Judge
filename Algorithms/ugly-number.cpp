2bc9a6858fca831356b30f27874fe5815862b267
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-08-24 04:18:34 -0400

    263. Ugly Number

--- Algorithms/ugly-number.cpp
+++ Algorithms/ugly-number.cpp
@@ -1,14 +1,14 @@
 // 263. Ugly Number
 // https://leetcode.com/problems/ugly-number/
+// https://discuss.leetcode.com/topic/21785/2-4-lines-every-language
 #include <iostream>
 using namespace std;
 class Solution {
 public:
 	bool isUgly(int num) {
-		if (num == 0) return false;
-		while (num % 2 == 0) num /= 2;
-		while (num % 3 == 0) num /= 3;
-		while (num % 5 == 0) num /= 5;
+		for (int i = 2; i < 6 && num; ++i)
+			while (num % i == 0)
+				num /= i;
 		return num == 1;
 	}
 };
