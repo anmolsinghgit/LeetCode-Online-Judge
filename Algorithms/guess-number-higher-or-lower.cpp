1b9caa9c1f86c4e4968bddf8a4d172fb22f7c287
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-08-25 18:18:16 -0400

    374. Guess Number Higher or Lower

--- Algorithms/guess-number-higher-or-lower.cpp
+++ Algorithms/guess-number-higher-or-lower.cpp
@@ -21,3 +21,26 @@ public:
 		return -1;
 	}
 };
+
+class Solution {
+public:
+	int guessNumber(int n) {
+		int low = 1;
+		int high = n;
+		while (low <= high) {
+			int mid1 = low + (high - low) / 3;
+			int mid2 = high - (high - low) / 3;
+			int result1 = guess(mid1);
+			int result2 = guess(mid2);
+			if (result1 == 0) return mid1;
+			if (result2 == 0) return mid2;
+			if (result1 < 0) high = mid1 - 1;
+			else if (result2 > 0) low = mid2 + 1;
+			else {
+				low = mid1 + 1;
+				high = mid2 - 1;
+			}
+		}
+		return -1;
+	}
+};
