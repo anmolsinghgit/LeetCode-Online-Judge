d8556834e3a9398f3d40c185b060a6eacf774144
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-08-30 06:54:45 -0400

    136. Single Number

--- Algorithms/single-number.cpp
+++ Algorithms/single-number.cpp
@@ -1,17 +1,27 @@
 // 136. Single Number
 // https://leetcode.com/problems/single-number/
 // https://discuss.leetcode.com/topic/1916/my-o-n-solution-using-xor
+// http://www.cplusplus.com/reference/functional/bit_xor/
 #include <iostream>
 #include <vector>
+#include <iterator>
+#include <algorithm>
+#include <functional>
 using namespace std;
 class Solution {
 public:
 	int singleNumber(vector<int>& nums) {
-		int result = 0;
-		for (const auto &i : nums) result ^= i;
-		return result;
+		return accumulate(begin(nums), end(nums), 0, bit_xor<int>());
 	}
 };
+// class Solution {
+// public:
+// 	int singleNumber(vector<int>& nums) {
+// 		int result = 0;
+// 		for (const auto &i : nums) result ^= i;
+// 		return result;
+// 	}
+// };
 int main(void) {
 	Solution solution;
 	vector<int> nums = {1, 1, 2, 2, 3};
