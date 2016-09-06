4fc1f280e31c1d1e1060ae1fe70e6a82faa4a34a
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-09-06 14:29:39 -0500

    217. Contains Duplicate

---
+++ Algorithms/contains-duplicate.cpp
@@ -0,0 +1,21 @@
+// 217. Contains Duplicate
+// https://leetcode.com/problems/contains-duplicate/
+// https://discuss.leetcode.com/topic/14944/single-line-c-solution-60ms
+#include <iostream>
+#include <vector>
+#include <set>
+#include <iterator>
+using namespace std;
+class Solution {
+public:
+	bool containsDuplicate(vector<int>& nums) {
+		return nums.size() > set<int>(begin(nums), end(nums)).size();
+	}
+};
+int main(void) {
+	Solution solution;
+	vector<int> nums = {1, 22, 33, 2, 2, 19};
+	cout << boolalpha << solution.containsDuplicate(nums) << "\tPassed\n";
+	cout << "\nPassed All\n";
+	return 0;
+}
