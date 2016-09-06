1b9875c9ed72387357c375c2dc55b5fa01d1072b
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-09-06 14:12:50 -0500

    392. Is Subsequence

---
+++ Algorithms/is-subsequence.cpp
@@ -0,0 +1,40 @@
+// 392. Is Subsequence
+// https://leetcode.com/problems/is-subsequence/
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+	bool isSubsequence(string s, string t) {
+		return this->isSubsequence(s.data(), t.data());
+	}
+	bool isSubsequence(const char* x, const char* y) {
+		while (*y) x += *x == *y++;
+		return !*x;
+	}
+};
+// class Solution {
+// public:
+// 	bool isSubsequence(string s, string t) {
+// 		return this->isSubsequence(s.c_str(), t.c_str());
+// 	}
+// 	bool isSubsequence(const char* x, const char* y) {
+// 		while (*y) x += *x == *y++;
+// 		return !*x;
+// 	}
+// };
+// class Solution {
+// public:
+// 	bool isSubsequence(string s, string t) {
+// 		size_t i = 0, j = 0;
+// 		while (j < t.size()) if (s[i] == t[j++]) i++;
+// 		return i == s.size();
+// 	}
+// };
+int main(void) {
+	Solution solution;
+	cout << boolalpha << solution.isSubsequence("abc", "ahbgdc") << "\tPassed\n";
+	cout << boolalpha << solution.isSubsequence("axc", "ahbgdc") << "\tPassed\n";
+	cout << "\nPassed All\n";
+	return 0;
+}
