13895c6ae619e616ddaeeba1da4cde30308080f6
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-09-13 14:33:14 -0500

    69. Sqrt(x)

--- Algorithms/sqrtx.cpp
+++ Algorithms/sqrtx.cpp
@@ -5,44 +5,53 @@
 using namespace std;
 class Solution {
 public:
-	int mySqrt(const int& x) {
-		if (x == 0) {
-			return 0;
-		}
-		size_t left(1);
-		size_t right(x);
-		size_t mid(left + (right - left) / 2);
-		while (left < right) {
-			if (mid * mid == x) {
-				return mid;
-			}
-			else if (mid * mid < x) {
-				if ((mid + 1) * (mid + 1) == x) {
-					return mid + 1;
-				}
-				else if ((mid + 1) * (mid + 1) > x) {
-					return mid;
-				}
-				else {
-					left = mid + 1;
-				}
-			}
-			else {
-				if ((mid - 1) * (mid - 1) == x) {
-					return mid - 1;
-				}
-				else if ((mid - 1) * (mid - 1) < x) {
-					return mid - 1;
-				}
-				else {
-					right = mid - 1;
-				}
-			}
-			mid = left + (right - left) / 2;
-		}
-		return mid;
+	int mySqrt(int x) {
+		size_t r = x;
+		while (r * r > (size_t)x)
+			r = (r + x / r) / 2;
+		return r;
 	}
 };
+// class Solution {
+// public:
+// 	int mySqrt(const int& x) {
+// 		if (x == 0) {
+// 			return 0;
+// 		}
+// 		size_t left(1);
+// 		size_t right(x);
+// 		size_t mid(left + (right - left) / 2);
+// 		while (left < right) {
+// 			if (mid * mid == x) {
+// 				return mid;
+// 			}
+// 			else if (mid * mid < x) {
+// 				if ((mid + 1) * (mid + 1) == x) {
+// 					return mid + 1;
+// 				}
+// 				else if ((mid + 1) * (mid + 1) > x) {
+// 					return mid;
+// 				}
+// 				else {
+// 					left = mid + 1;
+// 				}
+// 			}
+// 			else {
+// 				if ((mid - 1) * (mid - 1) == x) {
+// 					return mid - 1;
+// 				}
+// 				else if ((mid - 1) * (mid - 1) < x) {
+// 					return mid - 1;
+// 				}
+// 				else {
+// 					right = mid - 1;
+// 				}
+// 			}
+// 			mid = left + (right - left) / 2;
+// 		}
+// 		return mid;
+// 	}
+// };
 int main(void) {
 	Solution solution;
 	vector<int> a({2, 3, 7, 2147395599});
