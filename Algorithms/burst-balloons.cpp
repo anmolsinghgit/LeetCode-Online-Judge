51c590f7bd1f12ae9cc357e1947a1b5552b49021
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-09-16 15:56:52 -0500

    312. Burst Balloons

--- Algorithms/burst-balloons.cpp
+++ Algorithms/burst-balloons.cpp
@@ -6,6 +6,7 @@
 #include <iterator>
 #include <algorithm>
 using namespace std;
+// BEGIN: Bottom Up
 class Solution {
 public:
 	int maxCoins(vector<int>& nums) {
@@ -20,6 +21,27 @@ public:
 		return OPT[0][n - 1];
 	}
 };
+// END: Bottom Up
+// BEGIN: Top Down
+// class Solution {
+// public:
+// 	int maxCoins(vector<int>& nums) {
+// 		nums.insert(begin(nums), 1);
+// 		nums.insert(end(nums), 1);
+// 		const int n = nums.size();
+// 		vector<vector<int>> memorization(n, vector<int>(n, 0));
+// 		return this->maxCoins(nums, memorization, 0, n - 1);
+// 	}
+// private:
+// 	int maxCoins(vector<int>& nums, vector<vector<int>> &memorization, int i, int j) {
+// 		if (i >= j + 1) return 0;
+// 		if (memorization[i][j]) return memorization[i][j];
+// 		for (int k = i + 1; k < j; ++k)
+// 			memorization[i][j] = max(memorization[i][j], nums[i] * nums[k] * nums[j] + this->maxCoins(nums, memorization, i, k) + this->maxCoins(nums, memorization, k, j));
+// 		return memorization[i][j];
+// 	}
+// };
+// END: Top Down
 int main(void) {
 	Solution solution;
 	vector<int> nums = {3, 1, 5, 8};
