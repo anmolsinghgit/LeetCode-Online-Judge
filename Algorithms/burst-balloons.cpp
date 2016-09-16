3b82dcb5cd63cbd9bd08a1e0eebc177ca3b447ca
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-09-16 15:40:40 -0500

    312. Burst Balloons

--- Algorithms/burst-balloons.cpp
+++ Algorithms/burst-balloons.cpp
@@ -15,7 +15,7 @@ public:
 		vector<vector<int>> OPT(n, vector<int>(n, 0));
 		for (int len = 2; len < n; ++len)
 			for (int i = 0, j = i + len; j < n; ++i, ++j)
-				for (int k = i + 1; k <= j - 1; ++k)
+				for (int k = i + 1; k < j; ++k)
 					OPT[i][j] = max(OPT[i][j], OPT[i][k] + OPT[k][j] + nums[i] * nums[k] * nums[j]);
 		return OPT[0][n - 1];
 	}
