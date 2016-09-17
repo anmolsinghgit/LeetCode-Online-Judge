2d726c0585da2aa953d8d02b22e3209a7096f83e
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-09-16 20:06:38 -0500

    377. Combination Sum IV

--- Algorithms/combination-sum-iv.cpp
+++ Algorithms/combination-sum-iv.cpp
@@ -9,17 +9,32 @@ class Solution {
 public:
 	int combinationSum4(vector<int>& nums, int target) {
 		sort(begin(nums), end(nums));
-		while (!nums.empty() && nums.back() > target) nums.pop_back();
-		if (nums.empty()) return 0;
-		const int n = nums.size();
-		vector<vector<int>> OPT(target + 1, vector<int>(n, 0));
-		for (int i = 0; i < n; ++i) OPT[nums[i]][i] = 1;
-		for (int i = 1; i < target + 1; ++i)
-			for (int j = 0; j < n; ++j)
-				OPT[i][j] = i > nums[j] ? accumulate(begin(OPT[i - nums[j]]), end(OPT[i - nums[j]]), 0) : OPT[i][j];
-		return accumulate(begin(OPT.back()), end(OPT.back()), 0);
+		vector<int> OPT(target + 1, 0);
+		OPT[0] = 1;
+		for (int i = 1; i < target + 1; ++i) {
+			for (const auto &j : nums) {
+				if (j > i) break;
+				OPT[i] += OPT[i - j];
+			}
+		}
+		return OPT.back();
 	}
 };
+// class Solution {
+// public:
+// 	int combinationSum4(vector<int>& nums, int target) {
+// 		sort(begin(nums), end(nums));
+// 		while (!nums.empty() && nums.back() > target) nums.pop_back();
+// 		if (nums.empty()) return 0;
+// 		const int n = nums.size();
+// 		vector<vector<int>> OPT(target + 1, vector<int>(n, 0));
+// 		for (int i = 0; i < n; ++i) OPT[nums[i]][i] = 1;
+// 		for (int i = 1; i < target + 1; ++i)
+// 			for (int j = 0; j < n; ++j)
+// 				OPT[i][j] = i > nums[j] ? accumulate(begin(OPT[i - nums[j]]), end(OPT[i - nums[j]]), 0) : OPT[i][j];
+// 		return accumulate(begin(OPT.back()), end(OPT.back()), 0);
+// 	}
+// };
 int main(void) {
 	Solution solution;
 	vector<int> nums = {1, 2, 3};
