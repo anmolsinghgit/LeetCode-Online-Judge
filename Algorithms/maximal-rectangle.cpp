63e33cbf0e20d9abdfb6fadf07bfca4fe464969d
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-09-17 22:50:43 -0500

    85. Maximal Rectangle

--- Algorithms/maximal-rectangle.cpp
+++ Algorithms/maximal-rectangle.cpp
@@ -7,62 +7,47 @@
 using namespace std;
 class Solution {
 public:
-	int maximalRectangle(const vector<vector<char>>& matrix) {
-		if (matrix.empty()) {
-			return 0;
-		}
+	int maximalRectangle(vector<vector<char>>& matrix) {
 		const int m = matrix.size();
+		if (m == 0) return 0;
 		const int n = matrix.front().size();
-		vector<vector<int>> histgramMatrix(m, vector<int>(n, 0));
+		if (n == 0) return 0;
+		vector<vector<int>> Matrix(m, vector<int>(n, 0));
 		for (int i = 0; i < m; ++i) {
 			for (int j = 0; j < n; ++j) {
-				if (matrix[i][j] == '0') {
-					histgramMatrix[i][j] = 0;
+				if (matrix[i][j] == '0') continue;
+				if (i == 0) {
+					Matrix[i][j] = 1;
+					continue;
 				}
-				else {
-					histgramMatrix[i][j] = (i == 0 ? 1 : histgramMatrix[i - 1][j] + 1);
-				}
+				Matrix[i][j] = Matrix[i - 1][j] + 1;
 			}
 		}
 		int result = 0;
-		for (auto& i : histgramMatrix) {
-			result = max(result, this->largestRectangleArea(i));
-		}
+		for (auto &i : Matrix) result = max(result, this->largestRectangleArea(i));
 		return result;
 	}
 private:
-	int largestRectangleArea(vector<int>& nums) {
-		nums.push_back(0);
+	int largestRectangleArea(vector<int>& heights) {
+		heights.push_back(0);
 		int result = 0;
-		int i = 0;
 		vector<int> stack;
-		while (i < nums.size()) {
-			if (stack.empty() || nums[stack.back()] < nums[i]) {
+		for (int i = 0; i < (int)heights.size(); ) {
+			if (stack.empty() || heights[i] > heights[stack.back()]) {
 				stack.push_back(i++);
+				continue;
 			}
-			else {
-				int top = stack.back();
-				stack.pop_back();
-				result = max(result, nums[top] * (stack.empty() ? i : i - stack.back() - 1));
-			}
+			int top = stack.back();
+			stack.pop_back();
+			result = max(result, heights[top] * (stack.empty() ? i : (i - stack.back() - 1)));
 		}
 		return result;
 	}
 };
 int main(void) {
 	Solution solution;
-	vector<vector<char>> matrix = {
-									{'1', '0', '1', '0', '0'},
-									{'1', '0', '1', '1', '1'},
-									{'1', '1', '1', '1', '1'},
-									{'1', '0', '0', '1', '0'}
-									};
-	if (solution.maximalRectangle(matrix) == 6) {
-		cout << "\nPassed\n";
-	}
-	else {
-		cout << "\nError\n";
-		return 0;
-	}
+	vector<vector<char>> matrix = {{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
+	cout << solution.maximalRectangle(matrix) << "\tPassed\n";
+	cout << "\nPassed All\n";
 	return 0;
 }
