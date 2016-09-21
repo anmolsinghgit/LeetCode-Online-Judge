771f472a54222ccb71a3a44ab2ba9b2ccaa9996e
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-09-21 11:18:27 -0500

    300. Longest Increasing Subsequence

--- Algorithms/longest-increasing-subsequence.cpp
+++ Algorithms/longest-increasing-subsequence.cpp
@@ -1,5 +1,6 @@
 // 300. Longest Increasing Subsequence
 // https://leetcode.com/problems/longest-increasing-subsequence/
+// https://discuss.leetcode.com/topic/28696/9-lines-c-code-with-o-nlogn-complexity
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -8,19 +9,31 @@ using namespace std;
 class Solution {
 public:
 	int lengthOfLIS(vector<int>& nums) {
-		const int n = nums.size();
-		vector<int> OPT(n + 1, 0);
-		int result = 0;
-		for (int i = n - 1; i >= 0; --i) {
-			int tmp = 0;
-			for (int j = 1; i + j < n; ++j)
-				if (nums[i + j] > nums[i])
-					tmp = max(tmp, OPT[i + j]);
-			result = max(result, OPT[i] = (1 + tmp));
+		vector<int> result;
+		for (const auto& i : nums) {
+			vector<int>::iterator it = lower_bound(begin(result), end(result), i);
+			if (it == end(result)) result.push_back(i);
+			else *it = i;
 		}
-		return result;
+		return result.size();
 	}
 };
+// class Solution {
+// public:
+// 	int lengthOfLIS(vector<int>& nums) {
+// 		const int n = nums.size();
+// 		vector<int> OPT(n + 1, 0);
+// 		int result = 0;
+// 		for (int i = n - 1; i >= 0; --i) {
+// 			int tmp = 0;
+// 			for (int j = 1; i + j < n; ++j)
+// 				if (nums[i + j] > nums[i])
+// 					tmp = max(tmp, OPT[i + j]);
+// 			result = max(result, OPT[i] = (1 + tmp));
+// 		}
+// 		return result;
+// 	}
+// };
 int main(void) {
 	Solution solution;
 	vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
