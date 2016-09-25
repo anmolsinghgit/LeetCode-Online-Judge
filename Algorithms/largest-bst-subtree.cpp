21916b2ba1df4a6a224cfc7df49bcaf6abc3632a
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-09-25 15:48:23 -0500

    333. Largest BST Subtree

--- Algorithms/largest-bst-subtree.cpp
+++ Algorithms/largest-bst-subtree.cpp
@@ -19,15 +19,15 @@ public:
 	}
 private:
 	bool isValidBST(TreeNode* root) {
-		TreeNode *previous = NULL;
-		return this->isValidBST(root, previous);
+		TreeNode *predecessor = NULL;
+		return this->isValidBST(root, predecessor);
 	}
-	bool isValidBST(TreeNode* root, TreeNode*& previous) {
+	bool isValidBST(TreeNode* root, TreeNode*& predecessor) {
 		if (!root) return true;
-		if (!this->isValidBST(root->left, previous)) return false;
-		if (previous && previous->val >= root->val) return false;
-		previous = root;
-		if (!this->isValidBST(root->right, previous)) return false;
+		if (!this->isValidBST(root->left, predecessor)) return false;
+		if (predecessor && predecessor->val >= root->val) return false;
+		predecessor = root;
+		if (!this->isValidBST(root->right, predecessor)) return false;
 		return true;
 	}
 	int countNodes(TreeNode* root) {
