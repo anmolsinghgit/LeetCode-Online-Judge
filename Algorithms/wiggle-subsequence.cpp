d6da2a8483c9a798ccab1f5a66f4ef98e6ff2449
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-10-04 22:58:58 -0500

    376. Wiggle Subsequence

--- Algorithms/wiggle-subsequence.cpp
+++ Algorithms/wiggle-subsequence.cpp
@@ -4,24 +4,38 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+// BEGIN: https://discuss.leetcode.com/topic/51893/two-solutions-one-is-dp-the-other-is-greedy-8-lines
 class Solution {
 public:
 	int wiggleMaxLength(vector<int>& nums) {
 		if (nums.size() <= 1) return (int)nums.size();
-		vector<int> Up(nums.size(), 1);
-		vector<int> Down(nums.size(), 1);
-		int result = 0;
+		int Up = 1, Down = 1;
 		for (size_t i = 1; i < nums.size(); i++) {
-			for (size_t j = 0; j < i; ++j) {
-				if (nums[j] < nums[i]) Down[i] = max(Down[i], 1 + Up[j]);
-				if (nums[j] > nums[i]) Up[i] = max(Up[i], 1 + Down[j]);
-			}
-			result = max(result, Down[i]);
-			result = max(result, Up[i]);
+			if (nums[i - 1] < nums[i]) Up = Down + 1;
+			if (nums[i - 1] > nums[i]) Down = Up + 1;
 		}
-		return result;
+		return max(Down, Up);
 	}
 };
+// END: https://discuss.leetcode.com/topic/51893/two-solutions-one-is-dp-the-other-is-greedy-8-lines
+// class Solution {
+// public:
+// 	int wiggleMaxLength(vector<int>& nums) {
+// 		if (nums.size() <= 1) return (int)nums.size();
+// 		vector<int> Up(nums.size(), 1);
+// 		vector<int> Down(nums.size(), 1);
+// 		int result = 0;
+// 		for (size_t i = 1; i < nums.size(); i++) {
+// 			for (size_t j = 0; j < i; ++j) {
+// 				if (nums[j] < nums[i]) Down[i] = max(Down[i], 1 + Up[j]);
+// 				if (nums[j] > nums[i]) Up[i] = max(Up[i], 1 + Down[j]);
+// 			}
+// 			result = max(result, Down[i]);
+// 			result = max(result, Up[i]);
+// 		}
+// 		return result;
+// 	}
+// };
 int main(void) {
 	Solution solution;
 	vector<int> nums = {1,7,4,9,2,5};
