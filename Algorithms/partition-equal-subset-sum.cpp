5f3ea6f45476756ee16ce3c481ea2abb7765d950
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-10-09 16:30:38 -0500

    416. Partition Equal Subset Sum

--- Algorithms/partition-equal-subset-sum.cpp
+++ Algorithms/partition-equal-subset-sum.cpp
@@ -33,9 +33,7 @@ int main(void) {
 	nums = {1, 2, 3, 5};
 	cout << boolalpha << solution.canPartition(nums) << "\tPassed\n";
 	nums = {73, 75, 1, 61, 42, 92, 95, 94, 86, 81, 66, 21, 32, 80, 16, 52, 35, 90, 60, 20, 57, 95, 34, 40, 72, 83, 35, 67, 86, 78, 68, 47, 60, 72, 37, 83, 43, 35, 98, 24, 6, 38, 22, 42, 47, 62, 72, 42, 20, 70, 6, 75, 55, 28, 61, 85, 54, 33, 67, 99, 30, 80, 94, 56, 13, 33, 39, 37, 59, 82, 82, 16, 17, 60, 4, 36, 23, 76, 34, 45, 3, 97, 86, 2, 63, 17, 95, 92, 61, 36, 66, 18, 67, 5, 46, 38, 5, 53, 59, 17};
-	cout << boolalpha << solution.canPartition(nums) << "\tPassed\n";	
-	nums = {1, 5, 11, 5};
-	cout << boolalpha << solution.canPartition(nums) << "\tPassed\n";		
+	cout << boolalpha << solution.canPartition(nums) << "\tPassed\n";
 	cout << "\nPassed All\n";
 	return 0;
 }
