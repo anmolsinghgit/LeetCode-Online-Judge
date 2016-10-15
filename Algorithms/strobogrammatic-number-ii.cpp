d20beef66aa2ff3a6224b3e2d15ed3be6f135e33
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-10-14 23:50:36 -0500

    247. Strobogrammatic Number II

--- Algorithms/strobogrammatic-number-ii.cpp
+++ Algorithms/strobogrammatic-number-ii.cpp
@@ -13,12 +13,8 @@ public:
 private:
 	vector<string> findStrobogrammatic(int n, bool first) {
 		vector<string> result;
-		if (n == 0) return result;
-		if (n == 1) return result = {"0", "1", "8"};
-		if (n == 2) {
-			if (first) return result = {"11", "69", "88", "96"};
-			else return result = {"00", "11", "69", "88", "96"};
-		}		
+		if (n == 0) return result = {""};
+		if (n == 1) return result = {"0", "1", "8"};	
 		vector<pair<char, char>> X = {{'1', '1'}, {'6', '9'}, {'8', '8'}, {'9', '6'}};
 		vector<string> Y = this->findStrobogrammatic(n - 2, false);
 		for (const auto& i : Y)
@@ -31,6 +27,32 @@ private:
 		return result;
 	}
 };
+// class Solution {
+// public:
+// 	vector<string> findStrobogrammatic(int n) {
+// 		return this->findStrobogrammatic(n, true);
+// 	}
+// private:
+// 	vector<string> findStrobogrammatic(int n, bool first) {
+// 		vector<string> result;
+// 		if (n == 0) return result;
+// 		if (n == 1) return result = {"0", "1", "8"};
+// 		if (n == 2) {
+// 			if (first) return result = {"11", "69", "88", "96"};
+// 			else return result = {"00", "11", "69", "88", "96"};
+// 		}		
+// 		vector<pair<char, char>> X = {{'1', '1'}, {'6', '9'}, {'8', '8'}, {'9', '6'}};
+// 		vector<string> Y = this->findStrobogrammatic(n - 2, false);
+// 		for (const auto& i : Y)
+// 			for (const auto& j : X)
+// 				result.push_back(j.first + i + j.second);
+// 		if (!first) {
+// 			for (const auto& i : Y)
+// 				result.push_back('0' + i + '0');
+// 		}
+// 		return result;
+// 	}
+// };
 int main(void) {
 	Solution solution;
 	for (const auto &i : solution.findStrobogrammatic(2)) cout << i << '\t';
